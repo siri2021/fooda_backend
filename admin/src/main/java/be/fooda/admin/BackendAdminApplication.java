1631f62b8f94d40242907b6e5db1a29eee655441
yilmazchef <yilmaz@mail.be>
2020-07-29 22:16:06 +0200

    minor changes has been done

--- admin/src/main/java/be/fooda/admin/BackendAdminApplication.java
+++ admin/src/main/java/be/fooda/admin/BackendAdminApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EnableEurekaClient
+// @EnableAdminServer
 public class BackendAdminApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(BackendAdminApplication.class, args);
-	}
-
+	      SpringApplication.run(BackendAdminApplication.class, args);
+      }
 }
